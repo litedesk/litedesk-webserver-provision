1e28745b34936bfb86d0b161d01b44d49013a075
guymoller <guy.moller@gmail.com>
2014-11-10 10:22:20 +0100

    first try to build the django app with jenkins

---
+++ run_ci.sh
@@ -0,0 +1,20 @@
+#export DJANGO_SETTINGS_MODULE=cloudbook_host_server.settings.testing
+#export PYTHONPATH=$WORKSPACE/cloudbook_host_server
+
+virtualenv "$WORKSPACE/venv"
+source "$WORKSPACE/venv/bin/activate"
+
+if [ "$1" = "-profile" ]; then
+    PY_PROFILE="-m cProfile -o cprof.out"
+fi
+
+mkdir -p "$WORKSPACE/logs"
+
+# easy_install -U distribute || exit 23
+pip install --download-cache=/tmp -r $WORKSPACE/requirements.txt || exit 23
+
+python "$WORKSPACE/manage.py" reset_db --noinput --router=default || exit 23
+python "$WORKSPACE/manage.py" syncdb --noinput || exit 23
+python "$WORKSPACE/manage.py" migrate || exit 23
+#python $PY_PROFILE "$WORKSPACE/manage.py" jenkins core ccp password_reset manage_endusers update_interface || exit 23
+
