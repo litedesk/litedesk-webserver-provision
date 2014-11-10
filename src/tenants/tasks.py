e2e5dfc6df9a0355d0a7714499932aed29bb6ff6
Raphael Lullis <raphael@lullis.net>
2014-11-10 17:57:48 +0100

    Getting some error logging, load_users still is not that smart when registering users on okta.
    Signed-off-by: Raphael Lullis <raphael@lullis.net>

--- src/tenants/tasks.py
+++ src/tenants/tasks.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import logging
 import time
+
+log = logging.getLogger(__name__)
 
 def register_user_in_provisioning_service(service, user):
     # FIXME: The delay is necessary to avoid that services which depend on AD can get to see the newly created user
     time.sleep(4)
-    service.register(user)
+    try:
+        service.register(user)
+    except Exception, why:
+        log.warn('Failed when registering user %s on %s: %s' % (user, service, why))
