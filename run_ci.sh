c612a526059d90b73df75967c5e56fec59bf75fb
guymoller <guy.moller@gmail.com>
2014-11-12 10:30:21 +0100

    some more jenkins stuff

--- run_ci.sh
+++ run_ci.sh
@@ -13,6 +13,16 @@ mkdir -p "$WORKSPACE/logs"
 
 # easy_install -U distribute || exit 23
 pip install --download-cache=/tmp -r $WORKSPACE/requirements.txt || exit 23
+# download the fixtures
+git clone git@bitbucket.org:litedesk/cross7-data.git
+rm "$WORKSPACE/app.db"
+python "$WORKSPACE/src/manage.py" migrate || exit 23
+python "$WORKSPACE/src/manage.py" loaddata "$WORKSPACE/cross7-data/fixtures/app_bootstrap.json"  || exit 23
+python "$WORKSPACE/src/manage.py" load_users || exit 23
+pkill -f "manage.py runserver"
+python "$WORKSPACE/src/manage.py" runserver || exit 23
+
+
 
 #python "$WORKSPACE/src/manage.py" reset_db --noinput --router=default || exit 23
 #python "$WORKSPACE/src/manage.py" syncdb --noinput || exit 23
