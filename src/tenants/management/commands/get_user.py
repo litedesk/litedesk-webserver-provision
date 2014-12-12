c8c77d117893b6ed3d0f8c3c91a83871fa50176e
Philipp Offermann <philipp.offermann@telekom.de>
2014-12-12 13:54:55 +0100

    From get_user commend, remove airwatch smartgroup check till fix is available.
    Signed-off-by: Philipp Offermann <philipp.offermann@telekom.de>

--- src/tenants/management/commands/get_user.py
+++ src/tenants/management/commands/get_user.py
@@ -54,13 +54,13 @@ class Command(BaseCommand):
             result['airwatch']['id'] = airwatch_user.id
             result['airwatch']['Status'] = airwatch_user.Status
             result['airwatch']['applications'] = []
-            aw_assets = airwatch_service.airwatch.tenantserviceasset_set.all()
-            for asset in aw_assets:
-                smartgroup_id = asset.metadata['smartgroup_id']
-                if options["username"] in (
-                    user['Name'] for user in group.SmartGroup.get_remote(
-                        airwatch_client, smartgroup_id
-                    ).UserAdditions
-                ):
-                    result['airwatch']['applications'].append(asset.asset.name)
+            # aw_assets = airwatch_service.airwatch.tenantserviceasset_set.all()
+            # for asset in aw_assets:
+            #     smartgroup_id = asset.metadata['smartgroup_id']
+            #     if options["username"] in (
+            #         user['Name'] for user in group.SmartGroup.get_remote(
+            #             airwatch_client, smartgroup_id
+            #         ).UserAdditions
+            #     ):
+            #         result['airwatch']['applications'].append(asset.asset.name)
         self.stdout.write(json.dumps(result))
